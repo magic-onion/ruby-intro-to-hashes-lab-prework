53b3d8dcb6f416e241d30f073f09482838b56de5
Alexander Truong <alexander.m.truong@gmail.com>
2018-09-29 20:25:22 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,10 @@ def monopoly_with_second_tier
 	monopoly = {
 	  :railroads => {
 	    :pieces => 4,
-	    :names => {}
+	    :names => {},
+	    :rent_in_dollars => {
+	      :one_piece_owned => 25
+	    }
 	  }
 	}
 	
